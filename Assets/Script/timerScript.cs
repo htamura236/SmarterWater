d47e834577acaf7e385cb9bd2b76b325f15fd721
htamura236 <htamura@mail.csuchico.edu>
2022-04-03 11:47:48 -0700

    minor changes
    added explanation on the script

--- Assets/Script/timerScript.cs
+++ Assets/Script/timerScript.cs
@@ -9,22 +9,25 @@ public class timerScript : MonoBehaviour
     //Reference: https://gamedevbeginner.com/how-to-make-countdown-timer-in-unity-minutes-seconds/
 
   
-    public bool TimerOn;
+  
     public float timeRemaining = 10;
-    public bool timerIsRunning = false;
-
     public float seconds, minutes;
-
+    //bools to manege on/off of timer
+    public bool timerIsRunning = false;
+    public bool TimerOn;
+    //text messages on screen
     public Text countText;
     public Text startText;
     public Text gameEndText;
 
     void Start()
     {
+        //timer's format. you need to change "40" according to the time you set
         countText.text = string.Format("{0:00}:{0:40}", minutes, seconds);
+        //timer is off at the start point
         TimerOn = false;
         timerIsRunning = false;
-        //text
+        //messages are off at the start point
         startText.enabled = false;
         gameEndText.enabled = false;
 
@@ -41,9 +44,10 @@ public class timerScript : MonoBehaviour
     {
         if (other.gameObject.CompareTag("start"))
         {
+            //when player touched "start" game object, timer starts count down.
             Debug.Log("collided start object");
             other.gameObject.SetActive(false);
-
+            //show player game starts
             startText.enabled = true;
             startText.text = "Game Start!";
             //timer on
@@ -53,11 +57,13 @@ public class timerScript : MonoBehaviour
 
         if (other.gameObject.CompareTag("goal"))
         {
+            //when player touched "goal" game object, timer stop count down.
             Debug.Log("collided goal object");
             other.gameObject.SetActive(false);
-
+            //show a player game end
             gameEndText.enabled = true;
             gameEndText.text = "You Win!";
+            //timer off
             TimerOn = false;
             timerIsRunning = false;
 
@@ -95,10 +101,19 @@ public class timerScript : MonoBehaviour
         {
             timeRemaining = 0;
             timerIsRunning = false;
-            //let player know fish died, game is over, but the codes below doesn't work now
-            //Debug.Log("time over");
+            //let player know fish died, game is over, but the codes below doesn't work now.
+            //actually the log "time over" doesn't appear even when count became 0.
+
+            Debug.Log("time over");
             //gameEndText.enabled = true;
+            //gameObject.SetActive(false);
 
         }
+    }
+    void fishDie()
+    {
+        //show message "time over"
+        //deactivate player's game object so that player know he is no longer able to play game
+        //show menu so that player can choose "restart, or "back to menu"
     }
 }
