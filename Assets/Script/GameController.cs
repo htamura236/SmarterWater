54d38e82aed31c4a7fa8b14e80a32498a802a7de
VivianOverbey <95883554+VivianOverbey@users.noreply.github.com>
2022-04-17 03:01:58 -0700

    Game Manager and Menu Finished
    Medal Thresholds still need to be adjusted from tests

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -43,6 +43,7 @@ public class GameController : MonoBehaviour
 
 
 
+
     private static GameObject instance;
 
     // Start is called before the first frame update
--- Assets/Script/ScoreUpdater.cs
+++ Assets/Script/ScoreUpdater.cs
@@ -23,6 +23,42 @@ public class ScoreUpdater : MonoBehaviour
     [SerializeField]
     private bool TrophyL3;
 
+    [Header("Level 1 Medals")]
+    [SerializeField]
+    private Image[] MedalIconsLv1;
+    [SerializeField]
+    private int[] ThreshHoldsLv1;
+    [Header("Level 2 Medals")]
+    [SerializeField]
+    private Image[] MedalIconsLv2;
+    [SerializeField]
+    private int[] ThreshHoldsLv2;
+    [Header("Level 3 Medals")]
+    [SerializeField]
+    private Image[] MedalIconsLv3;
+    [SerializeField]
+    private int[] ThreshHoldsLv3;
+
+    [Header("Medal Point Display")]
+    [SerializeField]
+    private Text[] Level1;
+    [SerializeField]
+    private Text[] Level2;
+    [SerializeField]
+    private Text[] Level3;
+
+    [Header("Button")]
+    [SerializeField]
+    private GameObject level2Button;
+    [SerializeField]
+    private GameObject level3Button;
+    //0 bronze, 1 silver 2 gold
+    [SerializeField]
+    private int lv2Requirment;
+    [SerializeField]
+    private int lv3Requirment;
+
+
     private void Start()
     {
         //last level beat detection
@@ -59,5 +95,72 @@ public class ScoreUpdater : MonoBehaviour
         if(GameController.TrophyL2) { trophyIcons[1].gameObject.SetActive(true); }
         if(GameController.TrophyL3) { trophyIcons[2].gameObject.SetActive(true); }
 
+        //medals check
+        //level 1
+        if(GameController.highScores[0] >= ThreshHoldsLv1[0])
+        {
+            MedalIconsLv1[0].gameObject.SetActive(true);
+        }
+        if (GameController.highScores[0] >= ThreshHoldsLv1[1])
+        {
+            MedalIconsLv1[1].gameObject.SetActive(true);
+        }
+        if (GameController.highScores[0] >= ThreshHoldsLv1[2])
+        {
+            MedalIconsLv1[2].gameObject.SetActive(true);
+        }
+
+        //level 2
+        if (GameController.highScores[1] >= ThreshHoldsLv2[0])
+        {
+            MedalIconsLv2[0].gameObject.SetActive(true);
+        }
+        if (GameController.highScores[1] >= ThreshHoldsLv2[1])
+        {
+            MedalIconsLv2[1].gameObject.SetActive(true);
+        }
+        if (GameController.highScores[1] >= ThreshHoldsLv2[2])
+        {
+            MedalIconsLv2[2].gameObject.SetActive(true);
+        }
+
+        //level 3
+        if (GameController.highScores[2] >= ThreshHoldsLv3[0])
+        {
+            MedalIconsLv3[0].gameObject.SetActive(true);
+        }
+        if (GameController.highScores[2] >= ThreshHoldsLv3[1])
+        {
+            MedalIconsLv3[1].gameObject.SetActive(true);
+        }
+        if (GameController.highScores[2] >= ThreshHoldsLv3[2])
+        {
+            MedalIconsLv3[2].gameObject.SetActive(true);
+        }
+
+        //display of points required for diffrent medals
+        Level1[0].text = ThreshHoldsLv1[0] + " Points";
+        Level1[1].text = ThreshHoldsLv1[1] + " Points";
+        Level1[2].text = ThreshHoldsLv1[2] + " Points";
+        Level2[0].text = ThreshHoldsLv2[0] + " Points";
+        Level2[1].text = ThreshHoldsLv2[1] + " Points";
+        Level2[2].text = ThreshHoldsLv2[2] + " Points";
+        Level3[0].text = ThreshHoldsLv3[0] + " Points";
+        Level3[1].text = ThreshHoldsLv3[1] + " Points";
+        Level3[2].text = ThreshHoldsLv3[2] + " Points";
+        
+        //lv 2 check
+        if(GameController.highScores[0] < ThreshHoldsLv1[lv2Requirment])
+        {
+            level2Button.GetComponent<Button>().enabled = false;
+        }
+
+        //lv 3 check
+        if (GameController.highScores[1] < ThreshHoldsLv2[lv3Requirment])
+        {
+            level3Button.GetComponent<Button>().enabled = false;
+        }
     }
+
+
 }
