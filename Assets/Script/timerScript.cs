58de45a3568b8c7dce4416acc7049a02b0154233
VivianOverbey <voverbey@mail.csuchico.edu>
2022-05-01 14:29:45 -0700

    reverting

--- Assets/Script/timerScript.cs
+++ Assets/Script/timerScript.cs
@@ -46,6 +46,10 @@ public class timerScript : MonoBehaviour
     [SerializeField]
     private GameObject scoreScreen;
 
+    //death screen canvas
+    [SerializeField]
+    private GameObject deathScreen;
+
     [Header("Scorescreen Text")]
     //score screen text
     [SerializeField]
@@ -64,6 +68,9 @@ public class timerScript : MonoBehaviour
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 14ce2f3 (file broken)
     //youdied text fade in alpha
     private float currentAlpha = 0;
 =======
@@ -73,14 +80,31 @@ public class timerScript : MonoBehaviour
 
     public Text displayMessage;
 >>>>>>> Stashed changes
+<<<<<<< HEAD
 =======
 >>>>>>> parent of da9b694 (Death Screen Added to make death less jarring)
 =======
 >>>>>>> parent of da9b694 (Death Screen Added to make death less jarring)
+=======
+>>>>>>> parent of 14ce2f3 (file broken)
 
     void Awake()
     {
         timeAddedText.enabled = false;
+
+        //you died alpha setting
+
+        CanvasRenderer[] youDied;
+        youDied = new CanvasRenderer[2];
+        youDied[0] = deathScreen.transform.Find("Background").GetComponent<CanvasRenderer>();
+        youDied[1] = deathScreen.transform.Find("Background").transform.Find("YouDied").GetComponent<CanvasRenderer>();
+
+
+        foreach (CanvasRenderer item in youDied)
+        {
+            item.SetAlpha(0f);
+        }
+
 
         //timer's format. you need to change "40" according to the time you set
         countText.text = string.Format("{0:00}:{0:00}", minutes, seconds);
@@ -106,6 +130,8 @@ public class timerScript : MonoBehaviour
         GetComponent<fishRandomMovement>().enabled = false;
 
         startingTimeAmount = Mathf.RoundToInt(timeRemaining);
+
+        deathScreen.SetActive(false);
     }
 
     void Update()
@@ -265,7 +291,7 @@ public class timerScript : MonoBehaviour
                 }
                 else if(timeRemaining <= 0)
                 {
-                    Debug.Log("time over");
+                    //Debug.Log("time over");
                     fishDie();
                 }
             }
@@ -285,9 +311,34 @@ public class timerScript : MonoBehaviour
         //gameObject.SetActive(false);
         //respawn to the first position 
         //playerPos.position = new Vector3(respawnPoint.position.x, respawnPoint.position.y, respawnPoint.position.z);
-        DontDestroyOnLoad(GameObject.FindGameObjectWithTag("GameController"));
+
+
         GameController.score = 0;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        
+
+        deathScreen.SetActive(true);
+        if(deathScreen != null)
+        {
+            StartCoroutine("YouDiedFadeIn");
+        }
+
+        GameObject player = this.gameObject;
+        player.GetComponent<fishRandomMovement>().enabled = false;
+        player.GetComponent<playerControl>().enabled = false;
+        player.GetComponent<fishJumpControls>().enabled = false;
+        player.transform.Find("Main Camera").GetComponent<camraControl>().enabled = false;
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+        fishAnim.SetBool("Death", true);
+       
+
+        //DontDestroyOnLoad(GameObject.FindGameObjectWithTag("GameController"));
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+
 
         //show menu so that player can choose "restart, or "back to menu"
 
@@ -322,6 +373,9 @@ public class timerScript : MonoBehaviour
     }
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 14ce2f3 (file broken)
 
     private IEnumerator YouDiedFadeIn()
     {
@@ -361,8 +415,11 @@ public class timerScript : MonoBehaviour
     }
 
 >>>>>>> Stashed changes
+<<<<<<< HEAD
 =======
 >>>>>>> parent of da9b694 (Death Screen Added to make death less jarring)
 =======
 >>>>>>> parent of da9b694 (Death Screen Added to make death less jarring)
+=======
+>>>>>>> parent of 14ce2f3 (file broken)
 }
